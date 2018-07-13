4a2682587ec3aa0cf009e5fca4f9d6ba331a2f48
Bartłomiej Fatyga <bartlomiej254@wp.pl>
2018-07-13 22:07:08 +0200

    Dodanie wykresów kołowych

--- Main_Script.R
+++ Main_Script.R
@@ -1,5 +1,13 @@
 #Sprawdzanie i ładowanie bibliotek
 require("readr")
+require("dplyr")
+require("ggplot2")
+require("corrplot")
+require("tidyr")
+
+#=============================================
+#UWAGA INTERESUJE NAS TYLKO TRAINSET
+#=============================================
 
 #Sciezka
 Path <- getwd()
@@ -11,3 +19,27 @@ TestSet <- read_csv(paste(Path, "/Test.csv", sep=""))
 View(TestSet)
 Submission <- read_csv(paste(Path, "/Submission.csv", sep=""))
 View(Submission)
+
+#Statystyki opisowe
+summary(TrainSet)
+
+#Wykresy
+
+#Diagram kołowy typów produktów
+table(TrainSet$Item_Type)
+TrainSet %>% ggplot(aes(x = factor(1), fill = factor(Item_Type))) + geom_bar(width = 1) + coord_polar(theta = "y")
+
+#Diagram kołowy zawartosci tluszczow - widac dane do poprawy
+table(TrainSet$Item_Fat_Content)
+TrainSet %>% ggplot(aes(x = factor(1), fill = factor(Item_Fat_Content))) + geom_bar(width = 1) + coord_polar(theta = "y")
+
+#Diagram kołowy lokalizacji sklepów
+table(TrainSet$Outlet_Location_Type)
+TrainSet %>% ggplot(aes(x = factor(1), fill = factor(Outlet_Location_Type))) + geom_bar(width = 1) + coord_polar(theta = "y")
+
+#Diagram kołowy typu sklepów
+table(TrainSet$Outlet_Type)
+TrainSet %>% ggplot(aes(x = factor(1), fill = factor(Outlet_Type))) + geom_bar(width = 1) + coord_polar(theta = "y")
+
+#Wykres gęstości wagi przedmiotu
+TrainSet %>% ggplot(aes(x = Item_Weight)) + geom_density()
