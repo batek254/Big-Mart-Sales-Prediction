1e84abb1e3c93dd838345f315423341bd75dde9f
Paweł Klimczyk <pawelklimczyk.9@gmail.com>
2018-07-14 01:06:28 +0200

    skrypt1

---
+++ Skrypt_testowy_Pawel.R
@@ -0,0 +1,67 @@
+#Wczytanie bibliotek
+library(readr)
+
+###Sciezka
+Path <- getwd()
+
+##Wczytanie danych
+TrainSet <- read_csv(paste(Path, "/Train.csv", sep=""))
+View(TrainSet)
+TestSet <- read_csv(paste(Path, "/Test.csv", sep=""))
+View(TestSet)
+Submission <- read_csv(paste(Path, "/Submission.csv", sep=""))
+View(Submission)
+
+###wykresy
+
+
+##typ produktu
+
+
+#wykres
+
+table(TrainSet$Item_Type)
+as.data.frame(table(TrainSet$Item_Type))-> Typ_produktu_data_frame
+ggplot(data = Typ_produktu_data_frame, aes(x= Var1, y= Freq, group = 1))+
+  geom_bar(stat= 'identity',color = 'blue')+ labs(x= "produkt", y= 'częstosc zakupu')
+
+#podsumowanie
+summary(Typ_produktu_data_frame)
+
+
+
+##lokalizacja
+
+#wykres
+table(TrainSet$Outlet_Location_Type)
+as.data.frame(table(TrainSet$Outlet_Location_Type))-> Outlet_Localisation_data_frame
+ggplot(Outlet_Localisation_data_frame, aes(x= Var1 , y= Freq))+
+  geom_bar(stat ="identity", alpha= 0.6, color= 'green')+
+  labs(x = 'Tier', y = "ilość")
+
+
+
+##rozmiar
+
+
+#wykres kołowy
+table(TrainSet$Outlet_Size)
+TrainSet %>% ggplot(aes(x = factor(1), fill = factor(Outlet_Size))) + geom_bar(width = 1) + coord_polar(theta = "y")
+
+#wykres słupkowy
+ggplot(Outlet_Size_data_frame, aes(x= Var1 , y= Freq))+
+    geom_bar(stat ="identity", alpha= 0.9, color= 'green')+
+       labs(x = 'wielkosc', y = "ilość")
+
+
+##identyfikator_sklepu
+
+
+#wykres słupkowy
+as.data.frame(table(TrainSet$Outlet_Identifier))->Outlet_Identifier_data_frame
+ggplot(Outlet_Identifier_data_frame, aes(x= Var1 , y= Freq))+
+  geom_bar(stat ="identity", alpha= 0.9, color= 'green')+
+  labs(x = 'Outlet', y = "występywanie")
+
+#podsumowanie
+summary(Outlet_Identifier_data_frame)
